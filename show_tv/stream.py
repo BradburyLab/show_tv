792c79490fed89d87b369f58111500353ac7c249
Ilya Murav'jov <muravyev@bradburylab.com>
2013-12-10 20:25:45 +0400

    добавил тв-профили + обновил udp-input.yaml
    Conflicts:
    	config/example/udp-input.yaml
    	config/ilya/udp-source.yaml

--- show_tv/stream.py
+++ show_tv/stream.py
@@ -143,6 +143,13 @@ def make_chunk_options(t_p, chunk_dir, force_transcoding=False):
         "540": "-s 960x540 -b:v 1400k -level 3.1 -b:a 128k",
         "720": "-s 1280x720 -b:v 2500k -level 4.0 -b:a 128k",
         "1080": "-s 1920x1080 -b:v 6000k -level 5.0 -b:a 192k",
+        
+        # телевизионные (4:3), как у TightVideo
+        # уровни: http://en.wikipedia.org/wiki/H.264#Levels
+        # :TODO: -level и -b:a скопастен => надо правильный поставить
+        "240": "-s 320x240 -b:v 450k -level 2.1 -b:a 64k",
+        "480": "-s 640x480 -b:v 850k -level 3.1 -b:a 96k",
+        "576": "-s 720x576 -b:v 1300k -level 3.1 -b:a 128k",
     }
 
     # 44kHz - для HDS
